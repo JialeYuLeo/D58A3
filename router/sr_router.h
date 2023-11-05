e20c67a8fc92b2dfa4cad8da0cde85b3821b7aa6
Jiale Yu <78205690+JialeYuLeo@users.noreply.github.com>
2023-11-05 04:49:46 -0500

    length and checksum test

--- router/sr_router.h
+++ router/sr_router.h
@@ -85,4 +85,6 @@ void sr_set_ether_ip(struct sr_instance*, uint32_t);
 void sr_set_ether_addr(struct sr_instance*, const unsigned char*);
 void sr_print_if_list(struct sr_instance*);
 
+void forward_packet(struct sr_instance ,uint8_t, unsigned int)
+
 #endif /* SR_ROUTER_H */
